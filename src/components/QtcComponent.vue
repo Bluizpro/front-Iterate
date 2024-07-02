f2e11ce2ca177e4993e25e55f5d8ae625778aae6
Bluizpro <bluizprogramador@gmail.com>
2024-07-02 19:51:10 -0300

    testando bags

--- src/components/QtcComponent.vue
+++ src/components/QtcComponent.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <q-page>
     <div
       class="note-container"
@@ -215,6 +215,221 @@ const onReset = (index: string | number) => {
   margin-right: 1px !important;
 }
 
+.note-container {
+  border: 3px solid #ccc;
+  padding: 2rem;
+  width: 95%;
+  height: 100px;
+  align-items: flex-start;
+  margin: auto;
+  margin-top: 1rem;
+}
+.button-save,
+.button-done {
+  margin-left: 3px !important;
+  border: 1px solid #ccc;
+  padding: 5px;
+  border-radius: 4px;
+}
+.paginação {
+  margin-top: 2rem;
+}
+.custom-tooltip {
+  font-size: 1.5em; /* Ajuste este valor para o tamanho desejado */
+}
+</style>
+ -->
+
+<template>
+  <q-page>
+    <div
+      class="note-container"
+      v-for="(qtcInfor, index) in paginatedForms"
+      :key="index"
+    >
+      <q-form
+        @submit.prevent="onSubmit(index)"
+        class="q-gutter-md row items-center justify-start"
+      >
+        <q-input
+          disable
+          readonly
+          name="Data"
+          outlined
+          v-model="qtcInfor.data"
+          color="indigo-13"
+          label="Data"
+          class="col-2 data"
+        >
+          <template v-slot:prepend>
+            <q-icon name="date_range" />
+          </template>
+        </q-input>
+        <q-input
+          disable
+          readonly
+          name="Hora"
+          outlined
+          v-model="qtcInfor.hora"
+          color="indigo-13"
+          label="Hora"
+          class="col-2 hora"
+        >
+          <template v-slot:prepend>
+            <q-icon name="access_time" />
+          </template>
+        </q-input>
+        <q-input
+          outlined
+          v-model="qtcInfor.conjunto"
+          label="Conjunto"
+          class="col"
+        />
+        <q-input
+          disable
+          name="Usuario"
+          outlined
+          v-model="qtcInfor.usuario"
+          color="indigo-13"
+          label="Nome do usuário"
+          class="col-1 usuario"
+        >
+          <template v-slot:prepend>
+            <q-icon name="person" />
+          </template>
+        </q-input>
+        <q-input
+          outlined
+          v-model="qtcInfor.paciente"
+          label="Paciente"
+          color="indigo-13"
+          class="col-2 paciente"
+        />
+        <q-input
+          outlined
+          v-model="qtcInfor.informacoes"
+          label="Informações"
+          class="col"
+        >
+          <template v-slot:append>
+            <q-icon name="info">
+              <q-tooltip>
+                <div class="custom-tooltip">
+                  {{ qtcInfor.informacoes }}
+                </div>
+              </q-tooltip>
+            </q-icon>
+          </template>
+        </q-input>
+
+        <q-btn
+          icon="save"
+          color="primary"
+          flat
+          class="button-save"
+          size="10px"
+          @click="onSubmit(index)"
+        />
+        <q-btn
+          icon="done"
+          color="green"
+          flat
+          class="button-done"
+          size="10px"
+          @click="onReset(index)"
+        />
+      </q-form>
+    </div>
+    <div class="flex flex-center paginação">
+      <q-pagination v-model="currentPage" :max="maxPages" />
+    </div>
+  </q-page>
+</template>
+
+<script setup>
+import { computed, ref, watch } from 'vue';
+import { useQuasar } from 'quasar';
+const $q = useQuasar();
+const formsPerPage = 5;
+const currentPage = ref(1);
+const forms = ref(
+  JSON.parse(localStorage.getItem('qtcInfors')) || [
+    {
+      data: new Date().toLocaleDateString(),
+      hora: new Date().toLocaleTimeString(),
+      usuario: localStorage.getItem('usuarioLogado') || '',
+      paciente: '',
+      informacoes: '',
+      conjunto: '',
+    },
+  ]
+);
+watch(
+  forms,
+  () => {
+    localStorage.setItem('qtcInfors', JSON.stringify(forms.value));
+  },
+  { deep: true }
+);
+const maxPages = computed(() => Math.ceil(forms.value.length / formsPerPage));
+const paginatedForms = computed(() => {
+  const start = (currentPage.value - 1) * formsPerPage;
+  const end = start + formsPerPage;
+  return forms.value.slice(start, end);
+});
+const onSubmit = (index) => {
+  if (
+    forms.value[index].usuario === '' ||
+    forms.value[index].paciente === '' ||
+    forms.value[index].informacoes === '' ||
+    forms.value[index].conjunto === ''
+  ) {
+    $q.notify({
+      color: 'red-5',
+      textColor: 'white',
+      icon: 'warning',
+      message: 'Por favor, preencha todos os campos',
+    });
+  } else {
+    forms.value.push({
+      data: new Date().toLocaleDateString(),
+      hora: new Date().toLocaleTimeString(),
+      usuario: localStorage.getItem('usuarioLogado') || '',
+      paciente: '',
+      informacoes: '',
+      conjunto: '',
+    });
+    $q.notify({
+      color: 'green-4',
+      textColor: 'white',
+      icon: 'cloud_done',
+      message: 'Salvo Com Sucesso',
+    });
+  }
+};
+const onReset = (index) => {
+  const savedForms = JSON.parse(localStorage.getItem('savedInfors')) || [];
+  savedForms.push(forms.value[index]);
+  localStorage.setItem('savedInfors', JSON.stringify(savedForms));
+  if (forms.value.length > 1) {
+    forms.value.splice(index, 1);
+  } else {
+    forms.value[index].paciente = '';
+    forms.value[index].informacoes = '';
+    forms.value[index].conjunto = '';
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.row.items-start.justify-start {
+  flex-direction: row;
+  align-items: flex-start;
+  justify-content: flex-start;
+}
+.q-form .q-field {
+  margin-right: 1px !important;
+}
 .note-container {
   border: 3px solid #ccc;
   padding: 2rem;
